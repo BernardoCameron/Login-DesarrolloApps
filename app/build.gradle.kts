732113dbd229d51f997ea87ee46f63c65d955ad6
bernardoCameron <be.cameron@duocuc.cl>
2025-10-13 20:16:29 -0300

    implementando DAO

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,10 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+
+    // Agregar esto para habilitar KAPT
+
+    kotlin("kapt")
 }
 
 android {
@@ -57,6 +61,11 @@ dependencies {
 // Íconos (core opcional) y EXTENDIDOS (¡este es el clave!)
     implementation("androidx.compose.material:material-icons-core")
     implementation("androidx.compose.material:material-icons-extended")
+
+
+    implementation("androidx.room:room-runtime:2.6.1")  // Versión actualizada
+    kapt("androidx.room:room-compiler:2.6.1")          // Misma versión
+    implementation("androidx.room:room-ktx:2.6.1")     // Misma versión
 
 
     testImplementation(libs.junit)
--- app/src/main/java/com/example/login001v/navigation/AppNav.kt
+++ app/src/main/java/com/example/login001v/navigation/AppNav.kt
@@ -1,6 +1,8 @@
 package com.example.login001v.navigation
 
+import android.net.Uri
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavArgument
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -8,6 +10,8 @@ import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import com.example.login001v.ui.home.MuestraDatosScreen
 import com.example.login001v.ui.login.LoginScreen
+import com.example.login001v.view.DrawerMenu
+import com.example.login001v.view.ProductoFormScreen
 
 @Composable
 
@@ -23,7 +27,8 @@ fun AppNav(){
         }// fin composable 1
 
        composable(
-           route="muestraDatos/{username}",
+           //route="muestraDatos/{username}",
+           route="DrawerMenu/{username}",
            arguments = listOf(
                navArgument("username"){
                    type = NavType.StringType
@@ -34,10 +39,29 @@ fun AppNav(){
        {// inicio back
            backStackEntry->
            val username = backStackEntry.arguments?.getString("username").orEmpty()
-           MuestraDatosScreen(username=username, navController = navController)
+           //MuestraDatosScreen(username=username, navController = navController)
+           DrawerMenu(username=username, navController = navController)
+       }// fin back
+        //Enrutamiento para ProductroFormScreen
+        composable(
+            route="ProductoFormScreen/{nombre}/{precio}",
+            arguments = listOf(
+            navArgument("nombre"){type = NavType.StringType
+            },
+            navArgument("precio"){
+                type = NavType.StringType
+            }
+            )// fin list of
+        )// fin composable2
+        {// inicio back2
+                backStackEntry->
+            val nombre = Uri.encode(backStackEntry.arguments?.getString("nombre") ?: "")
 
-       }
+            val precio = backStackEntry.arguments?.getString("precio") ?:""
 
+            ProductoFormScreen( navController = navController, nombre=nombre, precio=precio)
+
+        }
 
     }// fin NavHost
 }// fin AppNav
